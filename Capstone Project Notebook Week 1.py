15d05b879ded02d0979afd7d25818f4a012bbc8f
amandapeggyoldridge <53192558+amandapeggyoldridge@users.noreply.github.com>
2019-07-23 13:08:58 -0700

    Add files via upload

---
+++ Capstone Project Notebook Week 1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Capstone Project Notebook
+# (This notebook will be mainly used for the capstone project)
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
